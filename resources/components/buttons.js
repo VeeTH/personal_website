65f6c321b1c9e7c0e1ca6f686ab10fa43cdc6899
VeeTHis <vicjthom1@gmail.com>
2024-09-17 19:08:22 -0400

    Add WIP projects, create under construction page, implement better autoplay system

--- resources/components/buttons.js
+++ resources/components/buttons.js
@@ -11,6 +11,7 @@ class ButtonList extends HTMLElement {
             <a href="https://thinliquid.dev" target="_blank"><img src="https://thinliquid.dev/thnlqd.png" alt="thinliquid's button" /></a>
             <a href="https://dimden.dev/" target="_blank"><img src="https://dimden.dev/services/images/88x31.gif"></a>
             <a href="https://milkyway.moe/" target="_blank"><img src="https://milkyway.moe/milkybuttons/milkybtn.gif"></a>
+            <a href="https://meteor.nekoweb.org/" target="_blank"><img src="https://meteor.nekoweb.org/button.gif"></a>
             <a href="https://modarchive.org/" target="_blank"><img src="resources/buttons/modarchive.png"></a>
 
             <img src="resources/buttons/queer-coded.png" style="padding-left: 50px;">
@@ -19,8 +20,9 @@ class ButtonList extends HTMLElement {
             <img src="resources/buttons/jesus.png">
             <a href="https://www.youtube.com/watch?v=qpFUWNrjFUw" target="_blank"><img src="resources/buttons/miku.gif"></a>
             <a href="https://necoarc.dev/" target="_blank"><img src="resources/buttons/necoarc.png"></a>
-            <a href="https://www.youtube.com/watch?v=eRbnVqTGLUc" target="_blank"><img src="resources/buttons/viburib.gif"></a> 
             <a href="https://reddit.com/r/touhou/comments/1f5r5i8/brace_yourself_cirno" target="_blank"><img src="resources/buttons/cirnosys.png"></a>
+            <a href="https://www.youtube.com/watch?v=eRbnVqTGLUc" target="_blank"><img src="resources/buttons/viburib.gif"></a> 
+            <img src="resources/buttons/mug.png">
          
             <a href="https://en.wikipedia.org/wiki/Windows_10" target="_blank"><img src="resources/buttons/made_with_windows.gif" style="padding-left: 50px;"></a>
             <a href="https://www.mozilla.org/firefox/" target="_blank"><img src="resources/buttons/firefox.gif"></a>
