ee7133d1998b692eebc88175d73c65b90c107927
VeeTHis <vicjthom1@gmail.com>
2024-10-20 18:29:12 -0400

    Start Blog page, add back button to Mobile page, clarify copyright

--- resources/components/footer.js
+++ resources/components/footer.js
@@ -7,8 +7,8 @@ class SiteFooter extends HTMLElement {
     this.innerHTML = `
             <span><img src="/resources/buttons/hand_coded_html.bmp" style="padding-top: 60px;"><img src="/resources/buttons/paws.png" style="padding-top: 60px; padding-left: 5px"></span>
             <h3>Powered by <a href="https://nekoweb.org/" target="_blank" style="color: plum !important; font-weight: normal !important; text-shadow: none !important;">nekoweb</a> :3</h3>
-            <h3>Last updated October 19th, 2024</h3>
-            <h3>Copyright © 2024<!---2025--> VeeTHis</h3>
+            <h3>Last updated October 20th, 2024</h3>
+            <h3>Copyright © 2024<!---2025--> VeeTHis (All original content)</h3>
     `;
   }
 }
