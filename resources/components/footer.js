be1bdc5c23eca78f8f0b8c3e335a76730a5f7592
VeeTHis <vicjthom1@gmail.com>
2024-10-30 14:45:56 -0400

    Add animated background using Vanta.js

--- resources/components/footer.js
+++ resources/components/footer.js
@@ -7,7 +7,7 @@ class SiteFooter extends HTMLElement {
     this.innerHTML = `
             <span><img src="/resources/buttons/hand_coded_html.bmp" style="padding-top: 60px;"><img src="/resources/buttons/paws.png" style="padding-top: 60px; padding-left: 5px"></span>
             <h3>Powered by <a href="https://nekoweb.org/" target="_blank" style="color: plum !important; font-weight: normal !important; text-shadow: none !important;">nekoweb</a> :3</h3>
-            <h3>Last updated October 25th, 2024</h3>
+            <h3>Last updated October 30th, 2024</h3>
             <h3>All original content © 2024<!---2025--> VeeTHis</h3>
     `;
   }
---
+++ resources/components/settings/vanta.js
@@ -0,0 +1,14 @@
+VANTA.FOG({
+    el: "#vantajs-bg",
+    mouseControls: false,
+    touchControls: false,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    highlightColor: 0x860808,
+    midtoneColor: 0xde1818,
+    lowlightColor: 0xd90000,
+    baseColor: 0x0,
+    blurFactor: 0.20,
+    speed: 0.4
+    })
