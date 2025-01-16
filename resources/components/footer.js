a82accef23d626b5f72d66a7146945173b057c5d
VeeTHis <vicjthom1@gmail.com>
2025-01-15 21:55:03 -0500

    Add icons to social links, add Xiaohongshu link

--- resources/components/footer.js
+++ resources/components/footer.js
@@ -7,7 +7,7 @@ class SiteFooter extends HTMLElement {
     this.innerHTML = `
             <span><img src="/resources/buttons/hand_coded_html.bmp" style="padding-top: 60px;"><img src="/resources/buttons/paws.png" style="padding-top: 60px; padding-left: 5px"></span>
             <h3>Powered by <a href="https://nekoweb.org/" target="_blank" style="color: plum !important; font-weight: normal !important; text-shadow: none !important;">nekoweb</a> :3</h3>
-            <h3>Last updated January 11th, 2025</h3>
+            <h3>Last updated January 15th, 2025</h3>
             <h3><a id="licensebox_btn" style="cursor: pointer; user-select: none;">Copyright © 2024<!---2025--> VeeTHis</a></h3>
 
             <div id="licensebox" title="LicenseBox" style="display: none;">
--- resources/components/socials.js
+++ resources/components/socials.js
@@ -6,15 +6,42 @@ class SocialsList extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <h1><b>Socials</b></h1>
-      <h3><!-- <img src="resources/icons/YouTube.svg" width=25rem> --><a href="https://youtube.com/c/veethis" target="_blank">YouTube</a></h3>
-      <h3><a href="https://reddit.com/u/veethis" target="_blank">Reddit (NSFW)</a></h3>
-      <h3><a href="https://tiktok.com/@veethis" target="_blank">TikTok</a></h3>
-      <h3><a href="https://instagram.com/vee_this" target="_blank">Instagram</a></h3>
-      <h3><a href="https://github.com/VeeTH" target="_blank">GitHub</a></h3>
-      <h3><a href="https://bsky.app/profile/veesdomain.xyz" target="_blank">Bluesky</a></h3>
-      <h3>Discord: <b>VeeTHis</b></h3>
-      <h3><a href="https://www.roblox.com/users/38524636/profile" target="_blank">Roblox</a></h3>
-      <h3><a href="https://www.anime-planet.com/users/VeeTHis" target="_blank">Anime Planet</a></h3>
+      <div class="social_link">
+        <img src="/resources/icons/YouTube.svg" width="25rem"></img>
+        <h3><a href="https://youtube.com/c/veethis" target="_blank">YouTube</a></h3>
+      </div>
+      <div class="social_link">
+        <img src="/resources/icons/Reddit_1.svg" width="25rem"></img>
+        <h3><a href="https://reddit.com/u/veethis" target="_blank">Reddit (NSFW)</a></h3>
+      </div>
+      <div class="social_link">
+        <img src="/resources/icons/TikTok.svg" width="25rem" style="zoom:90%; position: relative; left: 1px;"></img>
+        <h3><a href="https://tiktok.com/@veethis" target="_blank">TikTok</a></h3>
+      </div>
+      <div class="social_link">
+        <img src="/resources/icons/Instagram.svg" width="25rem"></img>
+        <h3><a href="https://instagram.com/vee_this" target="_blank">Instagram</a></h3>
+      </div>
+      <div class="social_link">
+        <img src="/resources/icons/Xiaohongshu.svg" width="25rem" style="zoom: 75%; position: relative; left: 4px;"></img>
+        <h3><a href="https://www.xiaohongshu.com/user/profile/6784b288000000000403ca40" target="_blank">Xiaohongshu</a></h3>
+      </div>
+      <div class="social_link">
+        <img src="/resources/icons/GitHub.svg" width="25rem"></img>
+        <h3><a href="https://github.com/VeeTH" target="_blank">GitHub</a></h3>
+      </div>
+      <div class="social_link">
+        <img src="/resources/icons/Bluesky.svg" width="25rem" style="zoom: 80%; position: relative; left: 4px;"></img>
+        <h3><a href="https://bsky.app/profile/veesdomain.xyz" target="_blank">Bluesky</a></h3>
+      </div>
+      <div class="social_link">
+        <img src="/resources/icons/Discord New.svg" width="25rem"></img>
+        <h3>Discord: <b>VeeTHis</b></h3>
+      </div>
+      <div class="social_link">
+        <img src="/resources/icons/Roblox.svg" width="25rem" style="zoom: 80%; position: relative; left: 4px;"></img>
+        <h3><a href="https://www.roblox.com/users/38524636/profile" target="_blank">Roblox</a></h3>
+      </div>
     `;
   }
 }
